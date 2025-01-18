53ecf7ce3e37ad6261151ae8196c9f429d65993d
Prasad2123 <prasadpilke21@gmail.com>
2025-01-18 17:28:48 +0530

    woring with header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,82 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import logo from "../assets/logo.png";
 
-const Header = () => {
+const drawerWidth = 240;
+const navItems = ["Booking", "Cars", "Contact"];
+
+export default function Header() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box sx={{ textAlign: "center", p: 2 }}>
+      <img src={logo} alt="Logo" style={{ height: "50px", width: "50px", marginBottom: "16px" }} />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ display: "flex"}}>
+      <AppBar component="nav" sx={{ backgroundColor: "#FFFFFF", color: "#000",boxShadow:"0" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+            sx={{ display: { sm: "none" }, mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="Logo" style={{ height: "4.3rem", width: "4.3rem", marginRight: "1rem" }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            GoRental
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Typography key={item} sx={{ color: "#000" , fontFamily:"Poppins"}}>
+                {item}
+              </Typography>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
 }
-
-export default Header
