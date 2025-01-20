ec270b472a93ae44f9f1325ba92d36215318e141
Kanchan <kanchanch0107@gmail.com>
2025-01-20 16:17:30 +0530

    body component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Body from "./components/body";
 
 const App=()=>{
   return(
    <>
    <Header/>
+   <Body/>
     <Outlet/>
    <Footer/>
    </>
