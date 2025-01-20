a0d076de938897e4107fecfcaf2590a9e46a75f6
Kanchan <kanchanch0107@gmail.com>
2025-01-20 16:20:38 +0530

    button added in main

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -24,7 +24,7 @@ export default function MediaCard() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
+        <Button size="small">Button </Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
